900944c30c69ad19a54d263cbec233d3fb9a3a44
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2015-06-09 21:03:50 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+function fromPairsReducer(obj, [key, value]) {
+  obj[key] = value
+  return obj
+}
+
+export function parseQuery(fullUrl) {
+  let query = fullUrl.split('?')[1]
+
+  if (!query) {
+    return {}
+  }
+
+  return query
+    .split('&')
+    .filter(q => q.trim().length > 0)
+    .map(q => q.split('=', 2))
+    .map(([key, value]) => [key, decodeURIComponent(value)])
+    .reduce(fromPairsReducer, {})
+}
+
+export default function matchRoutePattern(pattern, fullUrl) {
+  let url = fullUrl.split('?')[0]
+
+  let names = []
+  let regex = pattern
+    .replace(/:([^/]+)/g, name => {
+      names.push(name.replace(/^:/, ''))
+      return '([^/]+)'
+    })
+    .replace(/\//g, "\\/")
+
+  const r = new RegExp(`^${regex}$`)
+
+  if (!r.test(url)) {
+    return null;
+  }
+
+  const query = parseQuery(fullUrl)
+
+  const params = r.exec(url)
+    .slice(1)
+    .map((value, index) => [names[index], value])
+    .reduce(fromPairsReducer, {})
+
+  return Object.assign(query, params)
+}
